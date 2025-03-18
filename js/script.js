4cd767d269c56109e94b2540b971a02d9385a9c7
Kenny Lee <kenny.lee927@gmail.com>
2025-03-17 23:08:04 -0400

    create fetch function for pulling quiz data

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function fetchQuiz() {
+  fetch('https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=multiple')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const results = data.results;
+      const questionList = document.createElement("ol");
+
+      results.forEach(result => {
+        console.log(result.question);
+        const questionItem = document.createElement("li");
+        const questionOptions = document.createElement("ul"); 
+        
+        const questionOptionsArray = result.incorrect_answers;
+        questionOptionsArray.push(result.correct_answer);
+        
+        const questionOptionsContainer = document.createElement("ul");
+        questionOptionsArray.forEach(option => {
+          const choice = document.createElement("li");
+          choice.textContent = option;
+          questionOptionsContainer.appendChild(choice);
+        })
+        
+        questionItem.textContent = result.question;
+        questionList.appendChild(questionItem);        
+        questionItem.appendChild(questionOptionsContainer);
+      });
+
+      document.getElementById("response").appendChild(questionList);
+    })
+    .catch(error => console.error(error));
+}
