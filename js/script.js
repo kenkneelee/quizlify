bee0f7df0a2872bb073e7bb55928c513c43afe57
Kenny Lee <kenny.lee927@gmail.com>
2025-03-18 21:34:17 -0400

    delete unused code

--- js/script.js
+++ js/script.js
@@ -74,28 +74,3 @@ function startQuiz() {
         createForms(questions);
     });
 }
-
-// Creates a bullet point list of results
-// const questionList = document.createElement("ol");
-
-// results.forEach(result => {
-//   console.log(result.question);
-//   const questionItem = document.createElement("li");
-//   const questionOptions = document.createElement("ul");
-
-//   const questionOptionsArray = result.incorrect_answers;
-//   questionOptionsArray.push(result.correct_answer);
-
-//   const questionOptionsContainer = document.createElement("ul");
-//   questionOptionsArray.forEach(option => {
-//     const choice = document.createElement("li");
-//     choice.innerHTML = option;
-//     questionOptionsContainer.appendChild(choice);
-//   })
-
-//   questionItem.textContent = result.question;
-//   questionList.appendChild(questionItem);
-//   questionItem.appendChild(questionOptionsContainer);
-// });
-
-// document.getElementById("active").appendChild(questionList);
